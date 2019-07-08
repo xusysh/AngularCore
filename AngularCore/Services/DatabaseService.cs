d7328141e5669c50131f92732686d006997eb3dc
gjy gjy <xusysh@outlook.com>
2019-07-08 22:52:13 +0800

    添加项目文件。

---
+++ AngularCore/Services/DatabaseService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data.Entity;
+
+namespace AngularCore.Services
+{
+    public class DatabaseService
+    {
+        public MySqlConnection connection;
+        public DatabaseService(string server, string uid, string pwd, string database,
+            string charset = "utf-8", string SslMode = "None")
+        {
+            try
+            {
+                connection = new MySqlConnection($"server={server};uid={uid};pwd={pwd};database={database};" +
+                    $"charset={charset};SslMode={SslMode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Can't connect to database:{ex.Message}");
+                return;
+            }
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Can't open database connection:{ex.Message}");
+                return;
+            }
+        }
+        ~DatabaseService()
+        {
+            connection.CreateCommand();
+            connection.Close();
+        }
+    }
+}
---
+++ AngularCore/Services/LogService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularCore.Services
+{
+    public class LogService
+    {
+        StreamWriter file;
+        LogService(string filepath)
+        {
+            file = new StreamWriter(filepath);
+        }
+        ~LogService()
+        {
+            file.Close();
+        }
+        public void writeLog(string type, string content)
+        {
+            file.WriteLine($"{DateTime.Now}:\n{type}-{content}");
+        }
+
+    }
+    public class Log
+    {
+        public DateTime time { get; set; }
+        public string type { get; set; }
+        public string content { get; set; }
+    }
+}
